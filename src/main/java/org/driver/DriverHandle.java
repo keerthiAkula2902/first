36f1ee9595f16c4d8af2d7e2e487e99f4d9c2e4b
keerthiAkula2902 <keerthiakula2902@gmail.com>
2024-08-02 15:54:39 +0530

    Update DriverHandle.java

--- src/main/java/org/driver/DriverHandle.java
+++ src/main/java/org/driver/DriverHandle.java
@@ -15,7 +15,7 @@ import org.testng.annotations.Test;
 public class DriverHandle {
 
 	public static WebDriver driver=new ChromeDriver();
-	//driver.
+	//driver. handle
 	
 	@BeforeSuite
 	public void launch() {
